ff3cbfdb53ba2a82b71a45c97dd3bc2cfb5474d6
Michael Chen <michaelchengaming@gmail.com>
2019-10-30 10:30:18 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+
+  // Wubba Lubba Dub Dub!!
+  let el = document.createElement('h2')
+
+  document.body.appendChild(el)
+
+  document.querySelector('h2').innerText = 'Wubba Lubba Dub Dub!!!!!'
+
+
+
+  let text = document.getElementById('text')
+
+  text.textContent = "This is really cool!"
+
+});
+
